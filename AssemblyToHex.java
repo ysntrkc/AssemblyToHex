fe7339f0b916e17c137bf2c247cb847e5a39a403
ysntrkc <yasin.trkc.142@gmail.com>
2021-12-26 03:17:37 +0300

    Add required methods and organise the main func

--- AssemblyToHex.java
+++ AssemblyToHex.java
@@ -1,16 +1,24 @@
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
+// This program conversts assembly code to hexadecimal.
 public class AssemblyToHex {
 
     public static void main(String[] args) throws IOException {
-        File file = new File(args[0]);
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        File inputFile = new File(args[0]);
+        File outputFile = new File(args[1]);
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+
+        writer.write("v2.0 raw\n");
 
         String line;
-        while ((line = br.readLine()) != null) {
+        // Read each line of the file.
+        while ((line = reader.readLine()) != null) {
             String binary = "00";
             String isa[] = line.split(" ");
             String opcode = isa[0];
@@ -19,47 +27,79 @@ public class AssemblyToHex {
                 String secondPart[] = isa[1].split(",");
                 String dst = secondPart[0], src1 = secondPart[1], src2 = secondPart[2];
                 if (opcode.equals("ADD")) {
-                    binary += "000" + registerToBinString(dst) + registerToBinString(src1) + registerToBinString(src2);
-                    System.out.println(binary);
+                    binary += "000";
                 } else if (opcode.equals("AND")) {
-                    binary += "001" + registerToBinString(dst) + registerToBinString(src1) + registerToBinString(src2);
-                    System.out.println(binary);
+                    binary += "001";
                 } else if (opcode.equals("OR")) {
-                    binary += "010" + registerToBinString(dst) + registerToBinString(src1) + registerToBinString(src2);
-                    System.out.println(binary);
+                    binary += "010";
                 } else if (opcode.equals("XOR")) {
-                    binary += "011" + registerToBinString(dst) + registerToBinString(src1) + registerToBinString(src2);
-                    System.out.println(binary);
+                    binary += "011";
                 }
-
+                binary += registerToBinString(dst) + registerToBinString(src1) + "000" + registerToBinString(src2);
             } else if (opcode.equals("ADDI") || opcode.equals("ANDI") || opcode.equals("ORI")
                     || opcode.equals("XORI")) {
                 String secondPart[] = isa[1].split(",");
                 String dst = secondPart[0], src = secondPart[1], imm = secondPart[2];
-
+                if (opcode.equals("ADDI")) {
+                    binary += "000";
+                } else if (opcode.equals("ANDI")) {
+                    binary += "001";
+                } else if (opcode.equals("ORI")) {
+                    binary += "010";
+                } else if (opcode.equals("XORI")) {
+                    binary += "011";
+                }
+                binary += registerToBinString(dst) + registerToBinString(src) + "1" + decimalToTwosComp(imm, 6);
             } else if (opcode.equals("JUMP")) {
                 String address = isa[1];
-
+                binary += "110" + decimalToTwosComp(address, 15);
             } else if (opcode.equals("BEQ") || opcode.equals("BLT") || opcode.equals("BGT") || opcode.equals("BLE")
                     || opcode.equals("BGE")) {
                 String secondPart[] = isa[1].split(",");
                 String op1 = secondPart[0], op2 = secondPart[1], address = secondPart[2];
-
+                binary += "111";
+                if (opcode.equals("BEQ")) {
+                    binary += "010";
+                } else if (opcode.equals("BLT")) {
+                    binary += "100";
+                } else if (opcode.equals("BGT")) {
+                    binary += "001";
+                } else if (opcode.equals("BLE")) {
+                    binary += "110";
+                } else if (opcode.equals("BGE")) {
+                    binary += "011";
+                }
+                binary += registerToBinString(op1) + registerToBinString(op2) + decimalToTwosComp(address, 4);
             } else if (opcode.equals("LD")) {
                 String secondPart[] = isa[1].split(",");
                 String dst = secondPart[0], address = secondPart[1];
-
+                binary += "100" + registerToBinString(dst) + toBinary(Integer.parseInt(address), 11);
             } else if (opcode.equals("ST")) {
                 String secondPart[] = isa[1].split(",");
                 String src = secondPart[0], address = secondPart[1];
-
+                binary += "101" + registerToBinString(src) + toBinary(Integer.parseInt(address), 11);
             } else {
                 System.out.println("Error: Invalid opcode!");
                 System.exit(1);
             }
+            writer.write(binaryToHexString(binary) + "\n");
+            System.out.println(binaryToHexString(binary));
         }
+        writer.close();
     }
 
+    // This method converts a binary string to a hex string.
+    public static String binaryToHexString(String binary) {
+        String hex = "";
+        for (int i = 0; i < binary.length(); i += 4) {
+            String temp = binary.substring(i, i + 4);
+            int tempInt = Integer.parseInt(temp, 2);
+            hex += Integer.toHexString(tempInt).toUpperCase();
+        }
+        return hex;
+    }
+
+    // This method converts a number to binary string according to the given length.
     public static String toBinary(int num, int bits) {
         String binary = "";
         for (int i = 0; i < bits; i++) {
@@ -68,9 +108,61 @@ public class AssemblyToHex {
         return binary;
     }
 
+    // This method converts a register name to binary string.
     public static String registerToBinString(String register) {
         int registerNum = Integer.parseInt(register.substring(1));
         String rString = toBinary(registerNum, 4);
         return rString;
+    }
+
+    // This method converts a decimal to twos complemented binary string.
+    public static String decimalToTwosComp(String number, int bits) {
+        int num = Integer.parseInt(number);
+        // If number is zero or positive, return the binary string of the number.
+        if (num >= 0) {
+            return toBinary(num, bits);
+        }
+        // If the number is negative, return the twos complement of the number.
+        else {
+            String binary = toBinary(Math.abs(num), bits);
+            return twosComplement(binary);
+        }
+    }
+
+    // This method converts a binary string to twos complemented binary string.
+    public static String twosComplement(String bin) {
+        String twos = "", ones = "";
+
+        // First, flip the bits of the binary string.
+        for (int i = 0; i < bin.length(); i++) {
+            ones += flip(bin.charAt(i));
+        }
+
+        // Then, add 1 to the ones string.
+        int number0 = Integer.parseInt(ones, 2);
+        StringBuilder builder = new StringBuilder(ones);
+        boolean b = false;
+        for (int i = ones.length() - 1; i > 0; i--) {
+            if (ones.charAt(i) == '1') {
+                builder.setCharAt(i, '0');
+            } else {
+                builder.setCharAt(i, '1');
+                b = true;
+                break;
+            }
+        }
+
+        // If the first bit is 1, add 1 to the twos string.
+        if (!b)
+            builder.append("1", 0, 7);
+
+        twos = builder.toString();
+
+        return twos;
+    }
+
+    // Returns '0' for '1' and '1' for '0'
+    public static char flip(char c) {
+        return (c == '0') ? '1' : '0';
     }
 }
---
+++ README.md
@@ -0,0 +1,16 @@
+# AssemblyToHex
+
+The program converts assembly code to hexadecimal.
+
+---
+In order to run this program, you need to type some commands.
+
+* First, type the following command to compile the program:
+```
+javac AssemblyToHex.java
+```
+
+* Then, type the following command to run the program:
+```
+java AssemblyToHex <input_file> <output_file>
+```
